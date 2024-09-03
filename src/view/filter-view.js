4e6f1628674f66a94e4f94514ac3f0c4a6321d7b
Анна <anyaura@mail.ru>
2024-09-03 17:13:00 +0300

    ошибки

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -2,16 +2,16 @@ import {createElement} from '../render.js';
 
 const FILTER_TYPES = ['everything','future','present','past'];
 
-const createFilterItemsTemplate = (e) => `
+const createFilterItemsTemplate = (type) => `
   <div class="trip-filters__filter">
-    <input id="filter-${e}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${e}">
-    <label class="trip-filters__filter-label" for="filter-${e}">${e}</label>
+    <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}">
+    <label class="trip-filters__filter-label" for="filter-${type}">${type}</label>
   </div>
 `;
 
 const createFilterEventsTemplate = () => `
   <form class="trip-filters" action="#" method="get">
-    ${FILTER_TYPES.map( type => createFilterItemsTemplate(type)).join('')}
+    ${FILTER_TYPES.map((type) => createFilterItemsTemplate(type)).join('')}
     <button class="visually-hidden" type="submit">Accept filter</button>
   </form>
 `;
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -2,18 +2,20 @@ import {createElement} from '../render.js';
 
 const SORT_TYPES = ['day','event','time','price','offer'];
 
+const createSortBlockItemTemplate = (type) => `
+  <div class="trip-sort__item  trip-sort__item--${type}">
+    <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}">
+    <label class="trip-sort__btn" for="sort-${type}">${type}</label>
+  </div>
+`;
+
 const createSortBlockTemplate = () => `
   <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    ${SORT_TYPES.map( type => createSortBlockItemTemplate(type)).join('')}
+    ${SORT_TYPES.map((type) => createSortBlockItemTemplate(type)).join('')}
   </form>
 `;
 
-const createSortBlockItemTemplate = (e) => `
-  <div class="trip-sort__item  trip-sort__item--${e}">
-    <input id="sort-${e}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${e}">
-    <label class="trip-sort__btn" for="sort-${e}">${e}</label>
-  </div>
-`;
+
 
 export default class SortView {
   getTemplate() {
